8f03c9e085ebefaa8fc8c5f72921d8fd893d5dc9
71911 <100472383+71911@users.noreply.github.com>
2022-03-03 14:36:54 +0900

    Create parse.js

---
+++ parse.js
@@ -0,0 +1,38 @@
+
+      const homework = [
+        {
+          name: "과제2",
+          link: "/path"  
+        },
+      ]
+
+      const loop = () => {
+          
+      }
+
+      const _template = `
+        <div class="col">
+        <div class="card shadow-sm">
+          <div class="card-body">
+            <p class="card-text">[SUBJECT]</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary">보기</button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">저장소</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        `
+
+
+
+        const replaceHtml = (param0, param1) => {
+            let _output = _template.replace('[SUBJECT]', param0).replace('[LINK]', param1)
+            return _output;
+        }
+
+        for (let i in homework) {
+          document.getElementById('work').innerHTML = replaceHtml(homework[i].name, homework[i].link);
+        }
