ced733dbc8b8572f1e348275004ef8c697fc1201
archlabx <lawkr7x@gmail.com>
2022-03-08 16:58:15 +0900

    x

---
+++ array.js
@@ -0,0 +1,12 @@
+const homework = [
+  {
+    name: "과제1",
+    link: "html5/basicHtml.html",
+    repolink: "html5/basicHtml.html",
+  },
+   {
+    name: "테스트",
+    link: "html5/basicHtml.html",
+    repolink: "html5/basicHtml.html",
+  },
+]
---
+++ config.js
@@ -0,0 +1,18 @@
+
+         const template = `
+         <div class="col">
+         <div class="card shadow-sm">
+           <div class="card-body">
+             <p class="card-text">[SUBJECT]</p>
+             <div class="d-flex justify-content-between align-items-center">
+               <div class="btn-group">
+                 <a href="[LINK]" target="_blank" class="btn btn-sm btn-outline-secondary">보기</a>
+                 <!--<a href="[LINK]" class="btn btn-sm btn-outline-secondary">저장소</button>-->
+               </div>
+             </div>
+           </div>
+         </div>
+         </div>
+         `
+       const root = 'https://71911.github.io/';
+       const repo = 'https://github.com/71911/71911.github.io/tree/main/';
