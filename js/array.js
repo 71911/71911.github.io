ca7a253a5845a5a111c2595e5d25559eb749ec00
btcukra <btcforukraine@gmail.com>
2022-03-26 20:33:34 +0900

    3주차 인덱스 추가

--- js/array.js
+++ js/array.js
@@ -1,42 +1,81 @@
-const homework = [
-  {
+const homework = [{
     name: "과제1",
     week: 1,
     src: "basicHtml.html",
   },
-   {
+  {
     name: "애국가",
     week: 1,
     src: "anthem.html",
   },
   {
-   name: "def",
-   week: 2,
-   src: "def.html",
- },
+    name: "def",
+    week: 2,
+    src: "def.html",
+  },
   {
-   name: "loveKorea",
-   week: 2,
-   src: "loveKorea.html",
- },
- {
-  name: "globalAttribute",
-  week: 2,
-  src: "globalAttribute.html",
-},
-{
- name: "globalEvent",
- week: 2,
- src: "globalEvent.html",
-},
-{
- name: "headBasic",
- src: "headBasic.html",
- week: 2,
-},
-{
- name: "bodyBasic",
- week: 2,
- src: "bodyBasic.html",
-},
+    name: "loveKorea",
+    week: 2,
+    src: "loveKorea.html",
+  },
+  {
+    name: "globalAttribute",
+    week: 2,
+    src: "globalAttribute.html",
+  },
+  {
+    name: "globalEvent",
+    week: 2,
+    src: "globalEvent.html",
+  },
+  {
+    name: "headBasic",
+    src: "headBasic.html",
+    week: 2,
+  },
+  {
+    name: "bodyBasic",
+    week: 2,
+    src: "bodyBasic.html",
+  },
+  {
+    name: "basicHeading",
+    week: 3,
+    src: "basicHeading.html",
+  },
+  {
+    name: "basicPBrHr",
+    week: 3,
+    src: "basicPBrHr.html",
+  },
+  {
+    name: "basicChar",
+    week: 3,
+    src: "basicChar.html",
+  },
+  {
+    name: "basicList",
+    week: 3,
+    src: "basicList.html",
+  },
+  {
+    name: "basicDList",
+    week: 3,
+    src: "basicDList.html",
+  },
+  {
+    name: "basicTable",
+    week: 3,
+    src: "basicTable.html",
+  },
+  {
+    name: "tableSpan",
+    week: 3,
+    src: "tableSpan.html",
+  },
+  {
+    name: "advTable",
+    week: 3,
+    src: "advTable.html",
+  },
 ]
